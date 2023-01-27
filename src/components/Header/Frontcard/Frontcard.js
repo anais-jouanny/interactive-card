27b4396b129ae60e2b4e3a83808ad7b89c7addf3
anais-jouanny <anaisjouanny@gmail.com>
2023-01-27 10:54:09 +0100

    splice numb

--- src/components/Header/Frontcard/Frontcard.js
+++ src/components/Header/Frontcard/Frontcard.js
@@ -2,13 +2,21 @@ import PropTypes from 'prop-types';
 import './Frontcard.scss';
 
 function Frontcard({ infos }) {
+  const transformString = (string) => {
+    const newStr = string.split('');
+    newStr.splice(4, 0, ' ');
+    newStr.splice(9, 0, ' ');
+    newStr.splice(14, 0, ' ');
+    return newStr;
+  };
+
   return (
     <div className="card card-front">
       <div className="front-circles">
         <div className="circle-xl" />
         <div className="circle-xs" />
       </div>
-      <div className="front-number">{infos.number.split('').join(' ')}</div>
+      <div className="front-number">{transformString(infos.number)}</div>
       <div className="front-name">{infos.name}</div>
       <div className="front-date">
         <span className="month">{infos.month} </span>
